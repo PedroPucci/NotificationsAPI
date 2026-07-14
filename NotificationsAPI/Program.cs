3be234bfa052cceea848a5cf6a903accd2695c70
pedro <pedro@PEDROIGHORNOTEB>
2026-07-13 22:54:03 -0300

    ....

--- NotificationsAPI/Program.cs
+++ NotificationsAPI/Program.cs
@@ -1,3 +1,49 @@
+//using MassTransit;
+//using NotificationsAPI.Application.Abstractions.Services;
+//using NotificationsAPI.Application.Services;
+//using NotificationsAPI.Consumers;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddScoped<INotificationService, NotificationService>();
+
+//builder.Services.AddMassTransit(configuration =>
+//{
+//    configuration.AddConsumer<UserCreatedEventConsumer>();
+//    configuration.AddConsumer<PaymentProcessedEventConsumer>();
+
+//    configuration.UsingRabbitMq((context, rabbitMq) =>
+//    {
+//        rabbitMq.Host("localhost", "/", host =>
+//        {
+//            host.Username("guest");
+//            host.Password("guest");
+//        });
+
+//        rabbitMq.ReceiveEndpoint(
+//            "notifications-user-created",
+//            endpoint =>
+//            {
+//                endpoint.ConfigureConsumer<UserCreatedEventConsumer>(
+//                    context);
+//            });
+
+//        rabbitMq.ReceiveEndpoint(
+//            "notifications-payment-processed",
+//            endpoint =>
+//            {
+//                endpoint.ConfigureConsumer<PaymentProcessedEventConsumer>(
+//                    context);
+//            });
+//    });
+//});
+
+//var app = builder.Build();
+
+//app.MapGet("/", () => "NotificationsAPI is running.");
+
+//app.Run();
+
 using MassTransit;
 using NotificationsAPI.Application.Abstractions.Services;
 using NotificationsAPI.Application.Services;
@@ -5,7 +51,21 @@ using NotificationsAPI.Consumers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<INotificationService, NotificationService>();
+var rabbitMqHost =
+    builder.Configuration["RabbitMq:Host"]
+    ?? "localhost";
+
+var rabbitMqUsername =
+    builder.Configuration["RabbitMq:Username"]
+    ?? "guest";
+
+var rabbitMqPassword =
+    builder.Configuration["RabbitMq:Password"]
+    ?? "guest";
+
+builder.Services.AddScoped<
+    INotificationService,
+    NotificationService>();
 
 builder.Services.AddMassTransit(configuration =>
 {
@@ -14,10 +74,10 @@ builder.Services.AddMassTransit(configuration =>
 
     configuration.UsingRabbitMq((context, rabbitMq) =>
     {
-        rabbitMq.Host("localhost", "/", host =>
+        rabbitMq.Host(rabbitMqHost, "/", host =>
         {
-            host.Username("guest");
-            host.Password("guest");
+            host.Username(rabbitMqUsername);
+            host.Password(rabbitMqPassword);
         });
 
         rabbitMq.ReceiveEndpoint(
